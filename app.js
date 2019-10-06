b426507e49067c9d588d9e6d6ee3d52b796af775
Johann Woelper <woelper@gmail.com>
2019-10-06 23:09:00 +0200

    time delta

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
+
+// The plant component
 Vue.component('plant', {
     props: ['info'],
+    computed: {
+        abs_start: function() {
+            // return the absolute start date of an item.
+            return moment(moment().year() + "-" + this.info.start);
+        },
+        next_start: function() {
+            // return the absolute start date of an item.
+            var duration = moment.duration(moment().diff(this.abs_start)).asDays();
+            if (duration > 0) {
+                // return -moment.duration(moment().diff(this.abs_start.add(1,'years'))).asDays();
+                return moment(moment().add(1,'years').year() + "-" + this.info.start);
+            }            
+            return moment(moment().year() + "-" + this.info.start);
+        },
+        abs_start_diff: function() {
+            // return the difference as days. Add a year if it's in the past.
+            var duration = moment.duration(moment().diff(this.abs_start)).asDays();
+            if (duration > 0) {
+                // The correct way, but let's just add 365.
+                // return -moment.duration(moment().diff(this.abs_start.add(1,'years'))).asDays();
+                return -duration + 365;
+            }
+            return -duration
+        },
+    },
     template: `
         <div>
-            {{ info.name }}
             <img class="icon" :src="info.icon"></img>
+            {{ info.name }}
+            start {{abs_start.format('MMMM DD')}}
+            dist {{next_start.fromNow()}}
         </div>
     `
 })
@@ -12,13 +41,24 @@ Vue.component('plant', {
 var app = new Vue({
     el: '#app',
     data: {
+        settings: {
+            myplants: []
+        },
         now: null,
         sample_data: ["yellow", "blue", "green"],
         plants: plants
     },
-    mounted: function () {
-        var now = moment();
-        this.now = moment();
+    mounted() {
+        if (localStorage.settings) {
+            this.settings = localStorage.settings;
+        }
+
+        let year = moment().year();
+
+        // console.log(year);
+        // console.log(moment(year + "-03-08"));
+        
+
 
     },
     computed: {
@@ -27,8 +67,14 @@ var app = new Vue({
         },
     },
     methods: {
-        current_time: function () {
+        current_time: function() {
             return moment();
         },
-    }
+
+    },
+    watch: {
+        settings(new_settings) {
+          localStorage.settings = new_settings;
+        }
+      }
 })
--- data.js
+++ data.js
@@ -1,5 +1,5 @@
-plants = [
-    {
+plants = {
+    zwiebel: {
         name: "Zwiebel",
         soil: "Lehm",
         placement: "",
@@ -8,13 +8,64 @@ plants = [
         info: "",
         icon: "https://image.flaticon.com/icons/svg/123/123238.svg"
     },
-    {
+    tomate: {
         name: "Tomate",
         soil: "Lehm",
         placement: "",
-        start: "04-20",
+        start: "05-05",
         end: "08-01",
+        info: "",
+        icon: "https://image.flaticon.com/icons/svg/184/184540.svg"
+    },
+
+    karotte: {
+        name: "Karotte",
+        soil: "Lehm",
+        placement: "",
+        start: "03-01",
+        end: "05-10",
+        info: "",
+        icon: "https://image.flaticon.com/icons/svg/2098/2098914.svg"
+    },
+    test: {
+        name: "test",
+        soil: "Lehm",
+        placement: "",
+        start: "11-12",
+        end: "05-10",
+        info: "",
+        icon: "https://image.flaticon.com/icons/svg/2098/2098914.svg"
+    },
+    zucchini_plant: {
+        name: "Zucchinipflanze",
+        soil: "Lehm",
+        placement: "Ein Meter abstand, sonnig bis halbschattig, warm, feucht",
+        start: "05-20",
+        end: "01-09",
+        info: "",
+        icon: "https://image.flaticon.com/icons/svg/1202/1202127.svg"
+    },
+    zucchini_seed: {
+        name: "Zucchinisaat",
+        soil: "Lehm",
+        placement: "3 Zentimeter tief vorziehen, ca. 2 Wochen warten. Ein Meter abstand, sonnig bis halbschattig, warm, feucht",
+        start: "04-20",
+        end: "01-09",
+        info: "",
+        icon: "https://image.flaticon.com/icons/svg/1202/1202127.svg"
+    },
+    garlic: {
+        name: "Knoblauch",
+        soil: "Humus und Sand, keine Staunaesse",
+        placement: "4 Zentimeter tief. 10 cm abstand",
+        start: "09-15",
+        end: "01-09",
         info: "",
         icon: "https://image.flaticon.com/icons/svg/2166/2166244.svg"
     },
-];
+
+    
+};
+
+
+
